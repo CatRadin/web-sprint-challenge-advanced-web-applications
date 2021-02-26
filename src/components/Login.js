1f740a7a10a7e8fa8f28da392000e28ed8786ec2
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-02-26 13:55:20 -0600

    Form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,75 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useHistory } from 'react-router-dom'
+
+//Initial State Here -----------------------------------------------------
+const initialState = { 
+  username: '', 
+  password: ''}
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  useEffect(()=>{
+  //Use History----------------------------------------------------------
+  const history = useHistory();
+  //State Here -------------------------------------------------------------------------
+  const [creds, setCreds] = useState(initialState);
+  const [badLogin, setBadLogin] = useState(false);
+
+
+  //Handle Submit Here ---------------------------------------------------------
+  const handleSubmit = e => {
+    e.preventDefault();
     axios
-      .delete(`http://localhost:5000/api/colors/1`, {
-        headers:{
-          'authorization': "ahuBHejkJJiMDhmODZhZi0zaeLTQ4ZfeaseOGZgesai1jZWYgrTA07i73Gebhu98"
-        }
-      })
-      .then(res=>{
-        axios.get(`http://localhost:5000/api/colors`, {
-          headers:{
-            'authorization': ""
-          }
-        })
-        .then(res=> {
-          console.log(res);
-        });
-        console.log(res);
-      })
-  });
+    .post('http://localhost:5000/api/login', creds)
+    .then(res => {
+      localStorage.setItem('token',res.data.payload);
+      history.push('/bubbles')
+      setBadLogin(false);
+    })
+    .catch(err => setBadLogin(true));
+  };
+
+  //Handle Change Here ----------------------------------------------------------------
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setCreds({ ...creds, [name]: value });
+  }
 
   return (
-    <>
+    <div>
       <h1>
         Welcome to the Bubble App!
-        <p>Build a login page here</p>
       </h1>
-    </>
+      <form>
+        <h2>Login</h2>
+        <label>
+          Username: 
+          <input type='text' 
+          name='username' 
+          value={creds.username} 
+          onChange={handleChange} />
+        </label>
+        <label>
+          Password:
+          <input type='password' 
+          name='password' 
+          value={creds.password} 
+          onChange={handleChange} />
+        </label>
+        <button onClick={handleSubmit}>Login</button>
+      </form>
+      {badLogin && <p>Username or Password not valid.</p>}
+    </div>
   );
 };
 
 export default Login;
 
 //Task List:
-//1. Build a form containing a username and password field.
-//2. Add whatever state nessiary for form functioning.
-//3. MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEXT "username" and "password" RESPECTIVELY.
-//4. If either the username or password is not displaied display EXACTLY the following words: Username or Password not valid.
-//5. If the username / password is equal to Lambda School / i<3Lambd4, save that token to localStorage.
+//1. Build a form containing a username and password field. ✔️
+//2. Add whatever state nessiary for form functioning. ✔️
+//3. MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEXT "username" and "password" RESPECTIVELY. ✔️
+//4. If either the username or password is not displaied display EXACTLY the following words: Username or Password not valid. ✔️
+//5. If the username / password is equal to Lambda School / i<3Lambd4, save that token to localStorage. ✔️
