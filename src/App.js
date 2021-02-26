6658eb543434c8b6ba30cc9fdf54ffd0acd90973
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-02-26 13:19:03 -0600

    Private Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BubblePage from './components/BubblePage';
+import PrivateRoute from './components/PrivateRoute';
 import Login from "./components/Login";
 import "./styles.scss";
 
+
+//Set up a private route for the bubble page
 function App() {
   return (
     <Router>
       <div className="App">
+        <Switch>
+        <PrivateRoute path='/colors' component={BubblePage} />
         <Route exact path="/" component={Login} />
+        </Switch>
       </div>
     </Router>
   );
@@ -18,4 +24,4 @@ export default App;
 
 
 //Task List:
-//1. Render BubblePage as a PrivateRoute
+//1. Render BubblePage as a PrivateRoute ✔️
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,4 +1,26 @@
+import React, { Component } from 'react'
+import { Route, Redirect } from 'react-router-dom'
 
+
+//This will make sure that you can only reach the bubble page when logged in. Other wise you will be redirected to the Login page.
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    return (
+        <Route {...rest} render={
+            () => {
+                if (localStorage.getItem('token')){
+                    return (
+                        <Component />
+                    )
+                } else {
+                    return (
+                        <Redirect to='login' />
+                    )
+                }
+            }
+        } />
+    )
+}
+export default PrivateRoute
 
 
 
