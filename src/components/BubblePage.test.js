eb5c1e0e55d1a74259d474ca2debc64a3594ab24
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-02-26 14:30:12 -0600

    almost forgot tests!

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,28 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import { fetchColors as mockFetchColors } from '../helpers/fetchColors';
+
+jest.mock('../helpers/fetchColors');
+const testColors = {data: [{ color: 'pink', code: { hex: '#e48aba'}}, 
+{ color: 'green', code: { hex: '#27815f'}} ]};
 
 test("Renders BubblePage without errors", () => {
   // Finish this test
+  mockFetchColors.mockResolvedValueOnce(testColors);
+  render(<BubblePage/>);
 });
 
-test("Fetches data and renders the bubbles on mounting", () => {
+test("Fetches data and renders the bubbles on mounting", async () => {
   // Finish this test
+  mockFetchColors.mockResolvedValueOnce(testColors);
+  render(<BubblePage/>);
+
+  const pink = await screen.findByText(/pink/i);
+  expect(pink).toBeInTheDocument;
+
+  const green = await screen.findByText(/green/i);
+  expect(green).toBeInTheDocument;
 });
 
 //Task List
